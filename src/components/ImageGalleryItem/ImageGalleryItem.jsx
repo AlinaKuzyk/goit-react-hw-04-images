f6089883d85b84e9de93da4b9d6960c4731851dc
Alina_Kuzyk <dr.alinakuzyk@gmail.com>
2023-02-05 19:28:31 +0100

    add hooks for Modal, GalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,34 +1,56 @@
-import { Component } from 'react';
 import Modal from 'components/Modal/Modal';
 import css from './GalleryItem.module.css';
+import { useState } from 'react';
 
-class GalleryItem extends Component {
-  state = {
-    showModal: false,
+const GalleryItem = ({ webformatURL, tags, largeImageURL }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalToggle = () => {
+    setShowModal(!showModal);
   };
 
-  handleModalToggle = () => {
-    this.setState({ showModal: !this.state.showModal });
-  };
+  return (
+    <li className={css.imageGalleryItem}>
+      <img
+        className={css.imageGalleryItem_image}
+        src={webformatURL}
+        alt={tags}
+        onClick={handleModalToggle}
+      />
+      {showModal && (
+        <Modal largeImageURL={largeImageURL} onModalClose={handleModalToggle} />
+      )}
+    </li>
+  );
+};
 
-  render() {
-    return (
-      <li className={css.imageGalleryItem}>
-        <img
-          className={css.imageGalleryItem_image}
-          src={this.props.webformatURL}
-          alt={this.props.tags}
-          onClick={this.handleModalToggle}
-        />
-        {this.state.showModal && (
-          <Modal
-            largeImageURL={this.props.largeImageURL}
-            onModalClose={this.handleModalToggle}
-          />
-        )}
-      </li>
-    );
-  }
-}
+// class GalleryItem extends Component {
+//   state = {
+//     showModal: false,
+//   };
+
+//   handleModalToggle = () => {
+//     this.setState({ showModal: !this.state.showModal });
+//   };
+
+//   render() {
+//     return (
+//       <li className={css.imageGalleryItem}>
+//         <img
+//           className={css.imageGalleryItem_image}
+//           src={this.props.webformatURL}
+//           alt={this.props.tags}
+//           onClick={this.handleModalToggle}
+//         />
+//         {this.state.showModal && (
+//           <Modal
+//             largeImageURL={this.props.largeImageURL}
+//             onModalClose={this.handleModalToggle}
+//           />
+//         )}
+//       </li>
+//     );
+//   }
+// }
 
 export default GalleryItem;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,32 +1,63 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
+
 import css from './Modal.module.css';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscCloseModal);
-  }
+const Modal = ({ onModalClose, largeImageURL }) => {
+  useEffect(() => {
+    const handleEscCloseModal = event => {
+      if (event.code === 'Escape') {
+        onModalClose();
+      }
+    };
 
-  handleEscCloseModal = event => {
-    if (event.code === 'Escape') {
-      this.props.onModalClose();
-    }
-  };
+    window.addEventListener('keydown', handleEscCloseModal);
 
-  handleCloseOnBackdrop = event => {
+    return () => {
+      window.removeEventListener('keydown', handleEscCloseModal);
+    };
+  }, [onModalClose]);
+
+  const handleCloseOnBackdrop = event => {
     if (event.target === event.currentTarget) {
-      this.props.onModalClose();
+      onModalClose();
     }
   };
 
-  render() {
-    return (
-      <div className={css.overlay} onClick={this.handleCloseOnBackdrop}>
-        <div className={css.modal}>
-          <img src={this.props.largeImageURL} alt="big_picture" />
-        </div>
+  return (
+    <div className={css.overlay} onClick={handleCloseOnBackdrop}>
+      <div className={css.modal}>
+        <img src={largeImageURL} alt="big_picture" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleEscCloseModal);
+//   }
+
+//   handleEscCloseModal = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onModalClose();
+//     }
+//   };
+
+//   handleCloseOnBackdrop = event => {
+//     if (event.target === event.currentTarget) {
+//       this.props.onModalClose();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className={css.overlay} onClick={this.handleCloseOnBackdrop}>
+//         <div className={css.modal}>
+//           <img src={this.props.largeImageURL} alt="big_picture" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default Modal;
